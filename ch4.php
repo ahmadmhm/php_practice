a2ff2bf5ddb30facef9cbeb2a4c82fdccaf316b4
ahmad <ahmadmohammadi940@gmail.com>
2019-01-27 16:57:38 +0330

    complete the chapter 3 and other things

---
+++ ch4.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ahmad
+ * Date: 1/27/2019
+ * Time: 16:39
+ */
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ahmad
+ * Date: 1/27/2019
+ * Time: 16:53
+ */
+echo 'ahmad';
+?>
+
+<html>
+
+<head></head>
+<body>
+    <h4>the practices is here</h4>
+    <ul>
+        <li style="margin-bottom: 10px"><a href="ch3.php"> chapter 3</a></li>
+        <li style="margin-bottom: 10px"><a href="ch4.php"> chapter 4</a></li>
+    </ul>
+
+
+
+</body>
+</html>
