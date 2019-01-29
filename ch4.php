c9471ee5061b29d6a3d10a233a18d43586a63b0c
ahmad <ahmadmohammadi940@gmail.com>
2019-01-29 12:07:36 +0330

    chapter 4 - studing the expressions

--- ch4.php
+++ ch4.php
@@ -5,3 +5,20 @@
  * Date: 1/27/2019
  * Time: 16:39
  */
+echo "a: [" . (20 > 9) . "]<br>";
+echo "b: [" . (5 == 6) . "]<br>";
+echo "c: [" . (1 == 0) . "]<br>";
+echo "d: [" . (1 == 1) . "]<br>";
+
+echo "a: [" . TRUE . "]<br>";
+echo "b: [" . FALSE . "]<br>";
+echo '<hr/>';
+
+$myname = "Brian";
+$myage = 37;
+echo "a: " . 73 . "<br>"; // Numeric literal
+echo "b: " . "Hello" . "<br>"; // String literal
+echo "c: " . FALSE . "<br>"; // Constant literal
+echo "d: " . $myname . "<br>"; // String variable
+echo "e: " . $myage . "<br>"; // Numeric variable
+
