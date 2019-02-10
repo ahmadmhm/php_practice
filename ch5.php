1fdc64570cc5ef25e506609f210f6147f0404da3
ahmad <ahmadmohammadi940@gmail.com>
2019-02-10 14:20:13 +0330

    chapter 5 ended

--- ch5.php
+++ ch5.php
@@ -5,3 +5,87 @@
  * Date: 1/31/2019
  * Time: 11:39
  */
+/*
+$object = new User;
+
+print_r($object); echo "<br>";
+$object->name = "Joe";
+$object->password = "mypass";
+print_r($object); echo "<br>";
+$object->save_user();
+class User
+{
+    public $name, $password;
+
+
+     function __construct($param1, $param2)
+    {
+    // Constructor statements go here
+         $username = "Guest";
+    }
+    function __destruct()
+    {
+// Destructor code goes here
+    }
+    function save_user()
+    {
+        echo "Save User code goes here";
+    }
+
+    function get_password()
+    {
+        return $this->password;
+    }
+}
+*/
+/*
+Translate::lookup();
+class Translate
+{
+    const ENGLISH = 0;
+    const SPANISH = 1;
+    const FRENCH = 2;
+    const GERMAN = 3;
+// ...
+    static function lookup()
+    {
+        echo self::SPANISH;
+    }
+}
+*/
+/*
+$temp = new Test();
+echo "Test A: " . Test::$static_property . "<br>";
+echo "Test B: " . $temp->get_sp() . "<br>";
+echo "Test C: " . $temp->static_property . "<br>";
+class Test
+{
+    static $static_property = "I'm static";
+    function get_sp()
+    {
+        return self::$static_property;
+    }
+}
+*/
+
+$object = new Tiger();
+echo "Tigers have...<br>";
+echo "Fur: " . $object->fur . "<br>";
+echo "Stripes: " . $object->stripes;
+class Wildcat
+{
+    public $fur; // Wildcats have fur
+    function __construct()
+    {
+        $this->fur = "TRUE";
+    }
+}
+class Tiger extends Wildcat
+{
+    public $stripes; // Tigers have stripes
+    function __construct()
+    {
+        parent::__construct(); // Call parent constructor first
+        $this->stripes = "TRUE";
+    }
+}
--- index.php
+++ index.php
@@ -16,6 +16,8 @@ echo 'ahmad';
     <ul>
         <li style="margin-bottom: 10px"><a href="ch3.php"> chapter 3</a></li>
         <li style="margin-bottom: 10px"><a href="ch4.php"> chapter 4</a></li>
+        <li style="margin-bottom: 10px"><a href="ch5.php"> chapter 5</a></li>
+        <li style="margin-bottom: 10px"><a href="ch6.php"> chapter 6</a></li>
     </ul>
 
 
